3101af12908fa29c6a2d742968a6fa78db7c55fd
noahkel <noahkeltsch@outlook.com>
2026-09-01 15:47:18 +0200

    Update radon.py

--- src/radon.py
+++ src/radon.py
@@ -771,7 +771,7 @@ class MatrixRadonAdapter(_RadonBase):
         finally:
             astra.projector.delete(proj_id)
 
-        csr = csr.astype(self.dtype.name)
+        csr = csr.astype(np.float32 if self.dense else np.float64)
 
         # Full system matrix
         self._A = self._csr_to_torch(csr)
